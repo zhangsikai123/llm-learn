40c0e7e24c54614504a9e7b14243044c4c2c46f5
sikai zhang <sikai@biomap.com>
2023-10-01 14:12:25 +0800

    D  langchain_core/loaders.py

--- langchain_core/loaders.py
+++
@@ -1,5 +0,0 @@
-from langchain.document_loaders.recursive_url_loader import RecursiveUrlLoader
-
-loader = RecursiveUrlLoader(
-    url=url, max_depth=2, extractor=lambda x: Soup(x, "html.parser").text
-)
